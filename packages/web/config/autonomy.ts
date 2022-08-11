569fc5e4d5edbc867d1f2570a46e0a47b256bb48
m00n620 <gex6611@gmail.com>
2022-08-11 10:18:26 -0400

    fix: addresses

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -1,11 +1,11 @@
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "osmo17p0twl5p4r3f3u09lqluamm8260et8p9vwaudt2mp508508uaxnse0xlwj",
+    "osmo1m5e3fu3tjv7j5txuv65p89tpqruzh29le5e926fssax2tzcf59nqd9fmxf",
 };
 
 export const WRAPPER_ADDRESSES: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "osmo1g5kx37pu93ha2uk8c8w23d8s2m95a7pe8au9qnj2mjpy662vfqwsvvq9ec",
+    "osmo1zkgtuakfpxw00m7rn0e2aphtxe34znr2ewajr04ch6h4usmcf6tqdhvejw",
 };
