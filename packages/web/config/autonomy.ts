de8ff0d89af3e6b361284c64714e6122eb638625
Diego <diegochui96@gmail.com>
2023-04-03 22:16:46 -0700

    feat: add mainnet

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -11,13 +11,15 @@ export const SUBQUERY_BACKUP_ENDPOINTS: { [chainId: string]: string } = {
 };
 
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
-  'osmosis-1': '',
+  'osmosis-1':
+    'osmo1wg5qzw6yn88yz9kxtwvs36fmq5jxa03pg6zptvgte62hrlw0c4rqc9mjtf',
   'osmo-test-4':
     'osmo1vqsg6qlhrxygx33w6ujcmn6dzl5v4nakslza9htdc53m7t9ehjmszep60p',
 };
 
 export const WRAPPER_ADDRESSES: { [chainId: string]: string } = {
-  'osmosis-1': '',
+  'osmosis-1':
+    'osmo1gvkwn0yclk0cwjzeu65cnl0uq0msx9sq0zk24q9gmfrenwerxyps2w3x87',
   'osmo-test-4':
     'osmo1xusya0jgqz3wwwd6dsequdzcpqvy5prr0p4w5g69z92p9x6rv2gqfw4gxl',
 };
